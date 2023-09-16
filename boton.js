21efec51b648ca304d14e441555aa18bda1e49fd
bviotti <brunoviotti04@gmail.com>
2023-09-15 23:01:37 -0300

    Correción final de header con dos menu

--- boton.js
+++ boton.js
@@ -1,10 +1,10 @@
 let menu = document.querySelector('.menu-burger');
 
-function mostrar(){
-    menu.style.display="grid";
+function mostrar() {
+    menu.style.display = "grid";
 }
 
 
-function cerrar(){
-    menu.style.display="none";
+function cerrar() {
+    menu.style.display = "none";
 }
